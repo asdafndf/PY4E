118e4816e4b43b3c423b0a4aae772a73f3a09a73
asdafndf <109662927+asdafndf@users.noreply.github.com>
2023-03-10 23:02:36 +0100

    create

---
+++ excercise_8.2.py
@@ -0,0 +1,10 @@
+# %%
+fhand = open('mailbox-short.txt')
+count = 0
+for line in fhand:
+    words = line.split()
+    if len(words) < 3: continue
+    if words[0] != 'From' : continue
+    print(words[2])
+
+
