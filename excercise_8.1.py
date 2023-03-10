1910638a935f2ea169a9def04c7677f7883fcbfc
asdafndf <109662927+asdafndf@users.noreply.github.com>
2023-03-10 21:30:31 +0100

    create

---
+++ excercise_8.1.py
@@ -0,0 +1,8 @@
+# %%
+def chop(a_list):
+    del a_list[0]
+    del a_list[-1]
+
+def middle(a_list):
+    return a_list[1:-1]
+
---
+++ excercise_8.3.py
@@ -0,0 +1,9 @@
+# %%
+fhand = open('mailbox-short.txt')
+for line in fhand:
+    words = line.split()
+    if len(words) == 0 or words[0] != 'From':
+        continue
+    print(words[2])
+
+
